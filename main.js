2e4849ccf2b1c063af89c6893228515f2638edd0
Nicole Kimi Furukava <nicolekimimarques1@gmail.com>
2026-06-10 21:10:40 -0300

    mapeando os elementos do DOM e definindo a URL da MockAPI

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// Arquivo para código javascript
+const API_URL = "https://6a29d990f59cb8f65f1daa0e.mockapi.io/materiais";
+
+const inputNome = document.getElementById("input-nome");
+const inputQuantidade = document.getElementById("input-quantidade");
+const btnCadastrar = document.getElementById("btn-cadastrar");
+const listaMateriais = document.getElementById("lista-materiais");
+const formCadastro = document.getElementById("form-cadastro");
