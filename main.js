b0aede75e25255c6eed4cf0cfb6888453dc38f60
Nicole Kimi Furukava <nicolekimimarques1@gmail.com>
2026-06-10 21:16:05 -0300

    implementando a funcao buscarMateriais com o metodo GET para listar os itens

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ const inputQuantidade = document.getElementById("input-quantidade");
 const btnCadastrar = document.getElementById("btn-cadastrar");
 const listaMateriais = document.getElementById("lista-materiais");
 const formCadastro = document.getElementById("form-cadastro");
+
+async function buscarMateriais() {
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw new Error("Erro ao buscar dados");
+        
+        const materiais = await response.json();
+        renderizarTabela(materiais);
+    } catch (error) {
+        console.error("Erro no GET:", error);
+    }
+}
+
+function renderizarTabela(materiais) {
+    listaMateriais.innerHTML = ""; 
+    materiais.forEach(material => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${material.nome}</td>
+            <td>${material.quantidade}</td>
+        `;
+        listaMateriais.appendChild(tr);
+    });
+}
